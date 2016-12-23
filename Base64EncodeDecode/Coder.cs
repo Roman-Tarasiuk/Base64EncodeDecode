962f8cc4932112d2bbc4bfc0aa0416cdd3d82d73
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2016-12-23 15:25:52 +0200

    Initial commit.

---
+++ Base64EncodeDecode/Coder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Base64EncodeDecode
+{
+    class Coder
+    {
+        public static bool Encode(string inputPath, string outputPath)
+        {
+            if (!File.Exists(inputPath))
+            {
+                throw new ArgumentException("File '" + inputPath + "' does not exist or access is denided.");
+            }
+
+            byte[] bytes = null;
+
+            try
+            {
+                bytes = File.ReadAllBytes(inputPath);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Exception in File.ReadAllBytes(path)", e);
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    writer.Write(Convert.ToBase64String(bytes));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Exception in StreamWriter.Write(string)", e);
+            }
+
+            return true;
+        }
+
+        public static bool Decode(string inputPath, string outputPath)
+        {
+            if (!File.Exists(inputPath))
+            {
+                throw new ArgumentException("File '" + inputPath + "' does not exist or access is denided.");
+            }
+
+            string base64 = null;
+            try
+            {
+                using (StreamReader reader = new StreamReader(inputPath))
+                {
+                    base64 = reader.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Exception while opening/reading '" + inputPath + "' file.", e);
+            }
+
+            byte[] bytes = Convert.FromBase64String(base64);
+
+            try
+            {
+                File.WriteAllBytes(outputPath, bytes);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Exception in File.WriteAllBytes(outputPath, bytes)", e);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Base64EncodeDecode/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Base64EncodeDecode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!CheckAttributes(args))
+            {
+                ShowProgramUsage();
+                Console.WriteLine("Exit.");
+                return;
+            }
+
+            bool completeSuccessfully = false;
+
+            if (args[0] == "-encode")
+            {
+                completeSuccessfully = Coder.Encode(args[1], args[2]);
+            }
+            else // "-decode"
+            {
+                completeSuccessfully = Coder.Decode(args[1], args[2]);
+            }
+
+            if (completeSuccessfully)
+            {
+                Console.WriteLine("Success " + args[0].Substring(1) + ".");
+            }
+            else
+            {
+                Console.WriteLine("Failed " + args[0].Substring(1) + ".");
+                ShowProgramUsage();
+            }
+        }
+
+
+        #region ** Helper Methods
+
+        static bool CheckAttributes(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                return false;
+            }
+
+            if (args[0] != "-encode" && args[0] != "-decode")
+            {
+                return false;
+            }
+
+            if (!File.Exists(args[1]))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static void ShowProgramUsage()
+        {
+            Console.WriteLine(
+@"Program usage:
+ Base64EncodeDecode.exe -encode|-decode input_file_path output_file_path"
+                );
+        }
+
+        #endregion
+
+
+        #region ** Methods
+
+        static void Encode(string inputPath, string outputPath)
+        {
+             //Coder.Encode(inputPath)
+        }
+
+        #endregion
+    }
+}
